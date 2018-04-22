c2eb6f8ba5d29c8a2ee8d6fe4c933fed2d94f94f
Kerbyt Capote <kerbyt.capote@gmail.com>
2018-04-22 14:32:34 -0300

    models employee, position y controller position

--- models/task.js
+++ models/task.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var validTypesTasks = {
+    values: ['TYPE_DEPS', 'TYPE_SUBS', 'TYPE_AREA'],
+    message: '{VALUE} no es un tipo de tarea permitido'
+}
+
 const TaskSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, "El nombre de la tarea es necesario"]
+    },
+    name: {
+        type: String,
+        required: [true, "El nombre de la tarea es necesario"]
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'TYPE_DEPS',
+        enum: validTypesTasks
     },
     subTask: [{
         type: Schema.Types.ObjectId,
