02ffff73a025a42c051655399ba3089b5af5378d
Kerbyt Capote <kerbyt.capote@gmail.com>
2018-05-13 11:53:22 -0400

    cambios

--- controllers/employeeProjectController.js
+++ controllers/employeeProjectController.js
@@ -39,11 +39,46 @@ router.get('/', authentication.verifyToken, (req, res, next) => {
             });
 });
 
+router.get('/:recordActive', authentication.verifyToken, (req, res, next) => {
+
+    let pagination = req.query.pagination || 0;
+    pagination = Number(pagination);
+    let recordActive = req.params.recordActive;
+    recordActive = Boolean(recordActive);
+
+    EmployeeProject.find({ 'recordActive': recordActive })
+        .populate('employee')
+        .populate('project')
+        .skip(pagination)
+        .limit(constants.PAGINATION)
+        .exec(
+            (err, employeeProjects) => {
+                if (err) {
+                    return res.status(500).json({
+                        success: false,
+                        message: 'No se pueden consultar los empleados',
+                        errors: err
+                    });
+                } else {
+                    EmployeeProject.count({}, (err, totalRecords) => {
+                        res.status(200).write(JSON.stringify({
+                            success: true,
+                            employeeProjects: employeeProjects,
+                            totalRecords: employeeProjects.length,
+                            pagination: pagination
+                        }, null, 2));
+                        res.end();
+
+                    });
+                }
+            });
+});
+
 router.get('/project/:idProject', authentication.verifyToken, (req, res, next) => {
 
     let idProject = req.params.idProject;
 
-    EmployeeProject.find({ 'project': idProject })
+    EmployeeProject.find({ 'project': idProject, 'recordActive': true })
         .populate('employee')
         .populate('project')
         .exec(
@@ -72,7 +107,7 @@ router.get('/notproyect/', authentication.verifyToken, (req, res, next) => {
 
     let idProject = req.params.idProject;
 
-    Employee.find()
+    Employee.find({ 'recordActive': true })
         .populate('position')
         .skip(pagination)
         .limit(constants.PAGINATION)
@@ -81,47 +116,49 @@ router.get('/notproyect/', authentication.verifyToken, (req, res, next) => {
                 if (err) {
                     return res.status(500).json({
                         success: false,
-                        message: 'No se pueden consultar los empleados',
+                        message: 'No se pueden consultar los datos',
                         errors: err
                     });
                 } else {
-                    Employee.count({}, (err, totalRecords) => {
-                        res.status(200).write(JSON.stringify({
-                            success: true,
-                            employees: employees,
-                            totalRecords: totalRecords,
-                            pagination: pagination
-                        }, null, 2));
-                        res.end();
 
-                    });
+                    EmployeeProject.find({ 'recordActive': true })
+                        .populate('employee')
+                        .populate('project')
+                        .exec(
+                            (err, employeeProjects) => {
+                                if (err) {
+                                    return res.status(500).json({
+                                        success: false,
+                                        message: 'No se pueden consultar los datos',
+                                        errors: err
+                                    });
+                                } else {
+
+                                    let freeEmployees = [];
+                                    let found = false;
+                                    employees.forEach(employee => {
+                                        found = false;
+                                        employeeProjects.forEach(employeeProject => {
+                                            if (employee._id === employeeProject.employee._id) {
+                                                found = true;
+                                            }
+                                        });
+                                        if (!found) {
+                                            freeEmployees.push(employee);
+                                        }
+                                    });
+
+                                    res.status(200).write(JSON.stringify({
+                                        success: true,
+                                        freeEmployees: freeEmployees,
+                                        totalRecords: freeEmployees.length
+                                    }, null, 2));
+                                    res.end();
+                                }
+                            });
                 }
             });
 
-
-    EmployeeProject.find({ 'project': idProject })
-        .populate('employee')
-        .populate('project')
-        .exec(
-            (err, employeeProjects) => {
-                if (err) {
-                    return res.status(500).json({
-                        success: false,
-                        message: 'No se pueden consultar los empleados',
-                        errors: err
-                    });
-                } else {
-                    EmployeeProject.count({}, (err, totalRecords) => {
-                        res.status(200).write(JSON.stringify({
-                            success: true,
-                            employeeProjects: employeeProjects,
-                            totalRecords: employeeProjects.length
-                        }, null, 2));
-                        res.end();
-
-                    });
-                }
-            });
 });
 
 router.get('/project/:idProject/:recordActive', authentication.verifyToken, (req, res, next) => {
@@ -265,25 +302,45 @@ router.delete('/:id', authentication.verifyToken, (req, res, next) => {
 
     let id = req.params.id;
 
-    EmployeeProject.findByIdAndRemove(id, (err, employeeProject) => {
+    EmployeeProject.findById(id, (err, employeeProject) => {
         if (err) {
             return res.status(500).json({
                 success: false,
-                message: 'No se puede eliminar el registro',
+                message: 'No se puede actualizar el registro',
                 errors: err
             });
-        } else if (employeeProject) {
-            res.status(200).json({
-                success: true,
-                message: 'Operación realizada de forma exitosa',
-                employeeProject: employeeProject
-            });
-        } else {
+        }
+
+        if (!employeeProject) {
             return res.status(400).json({
                 success: false,
                 message: 'No existe un registro con el id: ' + id,
-                errors: { message: 'No se pudo encontrar el registro para eliminar' }
+                errors: { message: 'No se pudo encontrar el registro para actualizar' }
             });
+        } else {
+
+            employeeProject.employee = req.body.employee;
+            employeeProject.project = req.body.project;
+            employeeProject.startDate = req.body.startDate;
+            employeeProject.endDate = req.body.endDate;
+            employeeProject.recordActive = false;
+
+            employeeProject.save((err, employeeProject) => {
+                if (err) {
+                    return res.status(400).json({
+                        success: false,
+                        message: 'No se puede actualizar el registro',
+                        errors: err
+                    });
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        message: 'Operación realizada de forma exitosa.',
+                        employeeProject: employeeProject
+                    });
+                }
+            });
+
         }
     })
 });
