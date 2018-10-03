1251a167cfdd067f08c383e6012c92de0f086505
Kerbyt Capote <kerbyt.capote@gmail.com>
2018-10-02 21:28:07 -0300

    cambios

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/jcaControl'
+    database: 'mongodb://jca:jca2018@cluster0-shard-00-00-dk0og.mongodb.net:27017,cluster0-shard-00-01-dk0og.mongodb.net:27017,cluster0-shard-00-02-dk0og.mongodb.net:27017/jcaControl?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 }
